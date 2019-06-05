f7960a3a3199c0ae61bc05ad76f6f14049723329
Seonu Jang <seonujang92@gmail.com>
2019-06-06 01:36:18 +0900

    change package name using snake case

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from manual_sitemap.__version__ import version
+
+with open("README.md") as rm:
+    long_description = rm.read()
+
+setup(
+    name='manual-sitemap',
+    version=version,
+    author='SJQuant',
+    license="MIT",
+    author_email='seonujang92@gmail.com',
+    description=(
+        'manually create sitemap config and generate sitemap using it'
+    ),
+    url='https://github.com/sjquant/manual-sitemap',
+    long_description=long_description,
+    packages=['manual_sitemap'],
+    keywords=['sitemap', 'xml'],
+    entry_points={
+        "console_scripts": [
+            "manual-sitemap = manual_sitemap.main:create_sitemap"
+        ]
+    },
+    install_requires=['click>=7.0'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
