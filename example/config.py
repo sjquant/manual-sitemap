054cd1946eb51f44a618e8ca8aab69141556298a
Seonu Jang <seonujang92@gmail.com>
2019-06-06 01:47:07 +0900

    add comment

--- example/config.py
+++ example/config.py
@@ -1,10 +1,13 @@
 DOMAIN = 'https://example.com'
 
+# (route, [querie string keys])
+# queries must be emtpy list if it has no query strings.
 ROUTES = [
     ('/exercise', ['page', 'date']),
     ('/homework', ['date']),
 ]
 
+# { query string key: [query string values ]}
 QUERIES = {
     'page': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     'date': ['2019-01-01', '2019-04-03', '2019-04-08', '2019-05-06']
